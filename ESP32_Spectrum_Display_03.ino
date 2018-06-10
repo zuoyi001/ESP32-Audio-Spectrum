036c48d681c91abc1b0736c334bb0ed50e2a248f
tobozo <tobozo@users.noreply.github.com>
2018-06-10 18:12:11 +0200

    more refactoring
    - serial controls for the number of EQ bands
    - serial controls for the adc mode

--- ESP32_Spectrum_Display_03.ino
+++ ESP32_Spectrum_Display_03.ino
@@ -13,12 +13,11 @@
  * See more at http://dsbird.org.uk 
 */
 
+#include <driver/adc.h> // needed to get to adc1_get_raw()
 #include <Wire.h>
 #include <WiFi.h> // needed to disable the WiFi
 #include "arduinoFFT.h" // Standard Arduino FFT library: in IDE, Manage Library, then search for FFT 
 arduinoFFT FFT = arduinoFFT(); // or manually install from https://github.com/kosme/arduinoFFT
-
-#include <driver/adc.h> // needed to get to adc1_get_raw()
 
 
 // comment this out to use regular ILI9341
@@ -52,9 +51,23 @@ arduinoFFT FFT = arduinoFFT(); // or manually install from https://github.com/ko
   Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 #endif
 
-/////////////////////////////////////////////////////////////////////////
+#ifdef ILI9341_TFTWIDTH
+  #define TFT_WIDTH ILI9341_TFTWIDTH
+#else
+  #define TFT_WIDTH 320
+#endif
+#ifdef ILI9341_TFTHEIGHT
+  #define TFT_HEIGHT ILI9341_TFTHEIGHT
+#else
+  #define TFT_HEIGHT 240
+#endif
+
+#define TFT_HALFHEIGHT TFT_HEIGHT/2
+
+
 int SAMPLES = 256; // Must be a power of 2
-#define SAMPLING_FREQUENCY 40000 // Hz, must be 40000 or less due to ADC conversion time. Determines maximum frequency that can be analysed by the FFT Fmax=sampleF/2.
+#define SAMPLING_FREQUENCY 32000 // Hz, must be 40000 or less due to ADC conversion time. Determines maximum frequency that can be analysed by the FFT Fmax=sampleF/2.
+#define EQBANDS 8
 
 struct eqBand {
   const char *freqname;
@@ -67,83 +80,80 @@ struct eqBand {
   unsigned long lastmeasured;
 };
 
-eqBand audiospectrum[8] = {
+eqBand audiospectrum[EQBANDS] = {
   /* 
    * Adjust the amplitude/bandWidth values 
    * to fit your microphone
    */
-  { "125Hz", 500, 2,   0, 0, 0, 0, 0},
+  { "125Hz", 1000, 2,   0, 0, 0, 0, 0},
   { "250Hz", 500, 2,   0, 0, 0, 0, 0},
-  { "500Hz", 200, 3,   0, 0, 0, 0, 0},
-  { "1KHz",  200, 7,   0, 0, 0, 0, 0},
+  { "500Hz", 300, 3,   0, 0, 0, 0, 0},
+  { "1KHz",  250, 7,   0, 0, 0, 0, 0},
   { "2KHz",  200, 14,  0, 0, 0, 0, 0},
   { "4KHz",  100, 24,  0, 0, 0, 0, 0},
   { "8KHz",  50,  48,  0, 0, 0, 0, 0},
-  { "16KHz", 50,  155, 0, 0, 0, 0, 0}
+  { "16KHz", 25,  155, 0, 0, 0, 0, 0}
 };
 
 /* store bandwidth variations when sample rate changes */
-int bandWidth[8] = {
+int bandWidth[EQBANDS] = {
   0,0,0,0,0,0,0,0
 };
 
-
-/* waveform cache */
-int16_t eQGraph[320];
-float wmultiplier = 320 / SAMPLES;
-
+// sample helpers, buffer and data
 unsigned int sampling_period_us;
 unsigned long microseconds;
+double vReal[1024];
+double vImag[1024];
+unsigned long newTime;
+bool adcread = false; // use adc raw or analogread
 
-double vReal[4096];
-double vImag[4096];
-unsigned long newTime, oldTime;
-/////////////////////////////////////////////////////////////////////////
+// waveform cache and settings
+int16_t eQGraph[TFT_WIDTH];
+float wmultiplier = TFT_WIDTH / SAMPLES;
+bool wafeformdirtoggler = false;
 
-uint16_t tft_width = 320; // ILI9341_TFTWIDTH;
-uint16_t tft_height = 240; // ILI9341_TFTHEIGHT;
-
-uint8_t bands = 8;
-uint8_t bands_width = floor( tft_width / bands );
-uint8_t bands_pad = bands_width - 10;
+// EQ Bands settings
+uint8_t bands = EQBANDS;
+uint8_t bands_width = floor( TFT_WIDTH / bands );
+uint8_t bands_pad = bands_width - (TFT_WIDTH/16);
 uint16_t colormap[255]; // color palette for the band meter (pre-fill in setup)
 
-
-/* booleans to manage the display state */
+// booleans to manage the display state
 bool displayvolume = true;
 bool displaywaveform = true;
 bool displayspectrometer = true;
 
-
-
+// volume level
+long signalAvg = 0, signalMax = 0, signalMin = 4096;
+long max1 = 0, max2 = 0, then, now, nowthen;
 
 
 void drawAudioSpectrumGrid() {
   tft.setTextColor(ILI9341_YELLOW);
-  for(uint8_t i=0;i<tft_height;i++) {
-    colormap[i] = tft.color565(tft_height-i*.5, i*1.1, 0);
+  for(uint8_t i=0;i<TFT_HEIGHT;i++) {
+    colormap[i] = tft.color565(TFT_HEIGHT-i*.5, i*1.1, 0);
   }
-  for (byte band = 0; band <= 7; band++) {
+  for (byte band = 0; band < bands; band++) {
     tft.setCursor(bands_width*band + 2, 0);
     tft.print(audiospectrum[band].freqname);
   }
-  
 }
 
 
 void displayBand(int band, int dsize){
-  uint16_t hpos = bands_width*band;
+  uint16_t hpos = bands_width*band + (bands_pad/2);
   int dmax = 200;
-  if(dsize>tft_height-10) {
-    dsize = tft_height-10; // leave some hspace for text
+  if(dsize>TFT_HEIGHT-10) {
+    dsize = TFT_HEIGHT-10; // leave some hspace for text
   }
   if(dsize < audiospectrum[band].lastval) {
     // lower value, delete some lines
-    tft.fillRect(hpos, tft_height-audiospectrum[band].lastval, bands_pad, audiospectrum[band].lastval - dsize, ILI9341_BLACK);
+    tft.fillRect(hpos, TFT_HEIGHT-audiospectrum[band].lastval, bands_pad, audiospectrum[band].lastval - dsize, ILI9341_BLACK);
   }
   if (dsize > dmax) dsize = dmax;
   for (int s = 0; s <= dsize; s=s+4){
-    tft.drawFastHLine(hpos, tft_height-s, bands_pad, colormap[tft_height-s]);
+    tft.drawFastHLine(hpos, TFT_HEIGHT-s, bands_pad, colormap[TFT_HEIGHT-s]);
   }
   if (dsize > audiospectrum[band].peak) {
     audiospectrum[band].peak = dsize;
@@ -156,18 +166,18 @@ void displayBand(int band, int dsize){
 void setBandwidth() {
   byte multiplier = SAMPLES / 256;
   bandWidth[0] = audiospectrum[0].bandWidth * multiplier;
-  for(byte j=1;j<8;j++) {
+  for(byte j=1;j<bands;j++) {
      bandWidth[j] = audiospectrum[j].bandWidth * multiplier + bandWidth[j-1];
   }
-  wmultiplier = ((float)320 / (float)SAMPLES)*2;
+  wmultiplier = ((float)TFT_WIDTH / (float)SAMPLES)*2;
 }
 
 
 byte getBand(int i) {
-  for(byte j=0;j<8;j++) {
+  for(byte j=0;j<bands;j++) {
     if(i<=bandWidth[j]) return j;
   }
-  return 8;
+  return bands;
 }
 
 
@@ -182,24 +192,121 @@ void peakWaveForm() {
 
 void displayWaveForm(uint16_t color) {
   uint16_t lastx = 1;
-  uint16_t lasty = 120;
+  uint16_t lasty = TFT_HALFHEIGHT;
+  if(color!=ILI9341_BLACK) {
+    wafeformdirtoggler = !wafeformdirtoggler;
+  }
+  byte wafeformdirection = wafeformdirtoggler ? 1 : 0;
   for(uint16_t i=1;i<SAMPLES/2;i++) {
     if(eQGraph[i]!=0) {
       uint tmpy;
       uint nextx = i*wmultiplier;
-      if(i%2==1) {
-        tmpy = 120+eQGraph[i]/4;
+      if(i%2==wafeformdirection) {
+        tmpy = TFT_HALFHEIGHT+eQGraph[i]/3;
       } else {
-        tmpy = 120-eQGraph[i]/4;
+        tmpy = TFT_HALFHEIGHT-eQGraph[i]/3;
       }
       if(lasty!=0) {
+        //color = color==ILI9341_BLACK ? color : colormap[(byte)TFT_HEIGHT-(eQGraph[i])];
         tft.drawLine(lastx, lasty, nextx, tmpy, color);
       }
       lastx = nextx;
       lasty = tmpy;
     }
   }
-  tft.drawLine(lastx, lasty, 320, 120, color);
+}
+
+
+void captureSoundSample() {
+  signalAvg = 0;
+  signalMax = 0;
+  signalMin = 4096;
+  
+  for (int i = 0; i < SAMPLES; i++) {
+    newTime = micros();
+    if( adcread ) {
+      vReal[i] = adc1_get_raw( ADC1_CHANNEL_0 ); // A raw conversion takes about 20uS on an ESP32
+      delayMicroseconds(20);
+    } else {
+      vReal[i] = analogRead(A0); // A conversion takes about 1uS on an ESP32
+    }
+
+    vImag[i] = 0;
+    if(displayvolume) {
+      signalMin = min(signalMin, vReal[i]);
+      signalMax = max(signalMax, vReal[i]);
+      signalAvg += vReal[i];
+    }
+
+    while ((micros() - newTime) < sampling_period_us) { 
+      // do nothing to wait
+      yield();
+    }
+  }
+
+  FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+  FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
+  FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
+}
+
+
+void renderSpectrometer() {
+  if(displayvolume) {
+    signalAvg /= SAMPLES;
+    uint16_t vol = (signalMax - signalMin);
+    uint16_t volWidth = map(vol, 0, 4096, 1, TFT_WIDTH);
+    tft.drawFastHLine(0, 20, volWidth, ILI9341_GREEN);
+    tft.drawFastHLine(volWidth, 20, TFT_WIDTH-volWidth, ILI9341_BLACK);
+  }
+
+  if(displaywaveform) {
+    displayWaveForm(ILI9341_BLACK);
+    peakWaveForm();
+  }
+
+  for (int i = 2; i < (SAMPLES/2); i++){ // Don't use sample 0 and only first SAMPLES/2 are usable. Each array element represents a frequency and its value the amplitude.
+    if (vReal[i] > 1500) { // Add a crude noise filter, 10 x amplitude or more
+      byte bandNum = getBand(i);
+      if(bandNum!=bands) {
+        audiospectrum[bandNum].curval = (int)vReal[i]/audiospectrum[bandNum].amplitude;
+        if(displayspectrometer) {
+          displayBand(bandNum, audiospectrum[bandNum].curval);
+        }
+        if(displaywaveform) {
+          eQGraph[i] = audiospectrum[bandNum].curval;
+        }
+      }
+    }
+  }
+
+  if(displaywaveform) {
+    displayWaveForm(ILI9341_GREEN);
+  }
+  
+  if(displayspectrometer) {
+    long vnow = millis();
+    bool peakchanged = false;
+    for (byte band = 0; band < bands; band++) {
+      // auto decay every 50ms on low activity bands
+      if(vnow - audiospectrum[band].lastmeasured > 50) {
+        displayBand(band, audiospectrum[band].lastval>4 ? audiospectrum[band].lastval-4 : 0);
+      }
+      if (audiospectrum[band].peak > 0) {
+        audiospectrum[band].peak -= (band/3)+2;
+        if(audiospectrum[band].peak<=0) {
+          audiospectrum[band].peak = 0;
+        }
+      }
+      // only redraw peak if changed
+      if(audiospectrum[band].lastpeak != audiospectrum[band].peak) {
+        peakchanged = true;
+        // delete last peak
+        tft.drawFastHLine(bands_width*band+ (bands_pad/2), TFT_HEIGHT-audiospectrum[band].lastpeak, bands_pad, ILI9341_BLACK);
+        audiospectrum[band].lastpeak = audiospectrum[band].peak;
+        tft.drawFastHLine(bands_width*band+ (bands_pad/2), TFT_HEIGHT-audiospectrum[band].peak, bands_pad, colormap[TFT_HEIGHT-audiospectrum[band].peak]);
+      }
+    }
+  }
 }
 
 
@@ -209,9 +316,14 @@ void handleSerial() {
     char c = Serial.read();
     if(displaywaveform) {
       displayWaveForm(ILI9341_BLACK);
-      memset(eQGraph, 0, 320);
+      memset(eQGraph, 0, TFT_WIDTH);
     }
     switch(c) {
+      case 'a':
+        adcread = !adcread;
+        Serial.println("Use adc RAW " + String(adcread));
+        tft.fillScreen(ILI9341_BLACK);
+      break;
       case 'v':
         displayvolume = !displayvolume;
         Serial.println("Volume " + String(displayvolume));
@@ -237,6 +349,20 @@ void handleSerial() {
         }
         Serial.println("Sampling buffer " + String(SAMPLES));
       break;
+      case '*':
+        if( bands+1 <=8 ) bands++;
+        bands_width = floor( TFT_WIDTH / bands );
+        bands_pad = bands_width - (TFT_WIDTH/16);
+        tft.fillScreen(ILI9341_BLACK);
+        Serial.println("EQ Bands " + String(bands));
+      break;
+      case '/':
+        if( bands-1 >0 ) bands--;
+        bands_width = floor( TFT_WIDTH / bands );
+        bands_pad = bands_width - (TFT_WIDTH/16);
+        tft.fillScreen(ILI9341_BLACK);
+        Serial.println("EQ Bands " + String(bands));
+      break;
       default:
         //Serial.println("wut?");
       break;
@@ -245,6 +371,8 @@ void handleSerial() {
       drawAudioSpectrumGrid(); 
     }
     setBandwidth();
+    max1 = 0;
+    max2 = 0;
   }
 }
 
@@ -255,15 +383,14 @@ void setup() {
   
   adc1_config_width(ADC_WIDTH_12Bit);   //Range 0-1023 
   adc1_config_channel_atten(ADC1_CHANNEL_0,ADC_ATTEN_11db);  //ADC_ATTEN_DB_11 = 0-3,6V
+  sampling_period_us = round(1000000 * (1.0 / SAMPLING_FREQUENCY));
   
   tft.begin();
   tft.setRotation( 3 );
   tft.fillScreen(ILI9341_BLACK);
   tft.setTextColor(ILI9341_YELLOW);
   tft.setCursor(98, 42);
-  tft.print("loading");
-  
-  sampling_period_us = round(1000000 * (1.0 / SAMPLING_FREQUENCY));
+  tft.print("Sampling at: " + String(sampling_period_us) + "uS");
   
   delay(1000);
   
@@ -271,91 +398,16 @@ void setup() {
   
   drawAudioSpectrumGrid();
   setBandwidth();
-  memset(eQGraph, 0, 320);
+  memset(eQGraph, 0, TFT_WIDTH);
 }
+
+
 
 
 void loop() {
 
   handleSerial();
+  captureSoundSample();
+  renderSpectrometer();
 
-  long signalAvg = 0, signalMax = 0, signalMin = 4096;
-  
-  for (int i = 0; i < SAMPLES; i++) {
-    newTime = micros();
-    vReal[i] = adc1_get_raw( ADC1_CHANNEL_0 ); //analogRead(A0); // A conversion takes about 1uS on an ESP32
-    //vReal[i] = analogRead(A0); // A conversion takes about 1uS on an ESP32
-    delayMicroseconds(20);
-    vImag[i] = 0;
-    if(displayvolume) {
-      signalMin = min(signalMin, vReal[i]);
-      signalMax = max(signalMax, vReal[i]);
-      signalAvg += vReal[i];
-    }
-    while ((micros() - newTime) < sampling_period_us) { 
-      // do nothing to wait
-    }
-  }
-  
-  if(displayvolume) {
-    signalAvg /= SAMPLES;
-    uint16_t vol = (signalMax - signalMin);
-    uint16_t volWidth = map(vol, 0, 4096, 1, tft_width);
-    tft.drawFastHLine(0, 20, volWidth, ILI9341_GREEN);
-    tft.drawFastHLine(volWidth, 20, tft_width-volWidth, ILI9341_BLACK);
-  }
-  
-  FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
-  FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
-  FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
-
-  if(displaywaveform) {
-    displayWaveForm(ILI9341_BLACK);
-    peakWaveForm();
-  }
-
-  for (int i = 2; i < (SAMPLES/2); i++){ // Don't use sample 0 and only first SAMPLES/2 are usable. Each array eleement represents a frequency and its value the amplitude.
-    if (vReal[i] > 1500) { // Add a crude noise filter, 10 x amplitude or more
-      byte bandNum = getBand(i);
-      if(bandNum!=8) {
-        audiospectrum[bandNum].curval = (int)vReal[i]/audiospectrum[bandNum].amplitude;
-        if(displayspectrometer) {
-          displayBand(bandNum, audiospectrum[bandNum].curval);
-        }
-        if(displaywaveform && i<=160) {
-          eQGraph[i] = audiospectrum[bandNum].curval;
-        }
-      }
-    }
-  }
-
-  if(displaywaveform) {
-    displayWaveForm(ILI9341_GREEN);
-  }
-  
-  if(displayspectrometer) {
-    long vnow = millis();
-    bool peakchanged = false;
-    for (byte band = 0; band <= 7; band++) {
-      // auto decay every 50ms on low activity bands
-      if(vnow - audiospectrum[band].lastmeasured > 50) {
-        displayBand(band, audiospectrum[band].lastval>4 ? audiospectrum[band].lastval-4 : 0);
-      }
-      if (audiospectrum[band].peak > 0) {
-        audiospectrum[band].peak -= (band/3)+2;
-        if(audiospectrum[band].peak<=0) {
-          audiospectrum[band].peak = 0;
-        }
-      }
-      // only redraw peak if changed
-      if(audiospectrum[band].lastpeak != audiospectrum[band].peak) {
-        peakchanged = true;
-        // delete last peak
-        tft.drawFastHLine(bands_width*band, tft_height-audiospectrum[band].lastpeak, bands_pad, ILI9341_BLACK);
-        audiospectrum[band].lastpeak = audiospectrum[band].peak;
-        tft.drawFastHLine(bands_width*band, tft_height-audiospectrum[band].peak, bands_pad, colormap[tft_height-audiospectrum[band].peak]);
-      }
-    }
-  }
-  
 }
